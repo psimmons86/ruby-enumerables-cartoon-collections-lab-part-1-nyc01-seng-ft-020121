8a925ea8f3e9d185b156feafe00ca89847d257fb
patrick simmons <hadroncollides@gmail.com>
2021-01-25 22:03:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 
 
 def list_dwarves(array)
-  array.each do |dwarf|
+  array.each_with_index |dwarf|
     puts "Hello #{dwarf}!"
   end
   # Use `each_with_index` to enumerate over the provided array
