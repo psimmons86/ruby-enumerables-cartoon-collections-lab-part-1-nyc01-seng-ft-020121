ea78dcd55aa0aa63399a755ce8de31ddd20a1f48
patrick simmons <hadroncollides@gmail.com>
2021-01-25 22:06:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,6 @@ end
 def list_dwarves(array)
   array.each_with_index { |item, index| }
     array[item] = index
-  end
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
