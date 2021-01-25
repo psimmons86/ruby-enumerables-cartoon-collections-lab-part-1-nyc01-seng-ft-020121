6526f9bbd62d2085c3af4d760ef4e3dd846cdec5
patrick simmons <hadroncollides@gmail.com>
2021-01-25 22:23:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,9 @@ def list_dwarves(array)
   array.each_with_index do
     puts "#{index + 1} #{name}"
   end
+  list_dwarves
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-
+end
 
